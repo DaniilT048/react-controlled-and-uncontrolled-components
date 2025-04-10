28c8e9b7994a2f7c5623ded7b344d37a1e466098
DaniilT048 <tis.daniil98@gmail.com>
2025-04-10 13:58:05 +0300

    final

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -3,6 +3,8 @@ import {useEffect, useState} from 'react';
 export default function UserData() {
     const [userData, setUserData] = useState([]);
     const[userId, setUserId] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     const handleChangeUser = () => {
         if (userId > 9) {
@@ -15,22 +17,37 @@ export default function UserData() {
     }
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-            .then(res => res.json())
-            .then(data =>
-            {setUserData(data);
-            })
+        const fetchUser = async () => {
+            setLoading(true);
+            setError(null);
+
+            try {
+                const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+                if (!response.ok) {
+                    throw new Error('Error' + response.status);
+                }
+                const data = await response.json();
+                setUserData(data);
+            }catch(err) {
+                setError(err.message);
+                setUserData(null)
+            }finally {
+                setLoading(false);
+            }
+        };
+        fetchUser();
     }, [userId])
     return (
         <>
-            {userData ? (
+            {loading ? (
+                <p>Loading...</p>) : error ? (
+                    <p>{error}</p>
+                ): userData ? (
                 <div>
                     <p>Name: {userData.name}</p>
                     <p>Phone: {userData.phone}</p>
                 </div>
-            ): (
-                <p>Loading...</p>
-            )}
+                ): null}
             <button onClick={handleChangeUser}>Next user</button>
         </>
     )
